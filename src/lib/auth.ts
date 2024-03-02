a7dd26a7af66e3e0a80d7236cbc368868f9e66ef
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-03-02 10:18:30 -0500

    Pocketbase

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import PocketBase from 'pocketbase';
+
+const url = 'https://focus-ha.pockethost.io/'
+const client = new PocketBase(url);
+
+export function createUser() {
+
+}
+
+export function getUser() {
+
+}
