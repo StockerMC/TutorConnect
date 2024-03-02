2976e8ba1a8412874c1c429226c8cab34a5abd38
danielp1218 <danielpu2007@gmail.com>
2024-03-02 13:22:44 -0500

    navbar update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
 	import "../app.css";
 </script>
 
+<nav class="h-20 w-screen fixed top-0 left-0 bg-gradient-to-br from-blue-300 to-blue-100">
+    <div class="flex items-center h-full px-4">
+        <button class="text-center text-white font-bold pl-4 pr-4 h-[70%] bg-blue-950 rounded-2xl ml-auto align-middle">Get Started</button>
+    </div>
+</nav>
+
 <slot />
+
+<style>
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
-<nav class="h-16 w-screen bg-black"></nav>
 
 <div class="flex flex-col items-center justify-center h-screen w-screen bg-gray-100">
 	<h1 class="text-9xl font-bold text-gray-800">Home Page</h1>
