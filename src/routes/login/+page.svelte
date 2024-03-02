dd7e954074d9d345aab0aad3ed967ee05c404e77
Skilled5041 <skilled5041@gmail.com>
2024-03-02 10:53:23 -0500

    Added pocketbase types for users

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<div class="justify-center content-center items-center">
+    <form>
+        <div class="flex flex-col">
+            <label for="username">Enter your username:</label>
+            <input type="text" name="username" class="border-2"/>
+        </div>
+    </form>
+</div>
