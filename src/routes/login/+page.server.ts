1558b2bff75deec4ceb148ff329d50724dccb00b
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-03-02 11:06:29 -0500

    format

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,22 +2,22 @@ import type { Actions } from "./$types";
 import { fail } from "@sveltejs/kit";
 
 export const actions = {
-    login: async (event) => {
-        const data = await event.request.formData();
-        const username = data.get("username");
+	login: async (event) => {
+		const data = await event.request.formData();
+		const username = data.get("username");
 
-        if (!username || typeof username !== "string") {
-            return fail(400, {
-                error: "username is required"
-            });
-        }
+		if (!username || typeof username !== "string") {
+			return fail(400, {
+				error: "username is required"
+			});
+		}
 
-        event.cookies.set("username", username, {
-            path: "/"
-        });
+		event.cookies.set("username", username, {
+			path: "/"
+		});
 
-        return {
-            success: true
-        };
-    },
+		return {
+			success: true
+		};
+	}
 } satisfies Actions;
