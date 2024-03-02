8acacb84bb3fbe4fa2a2c1e46ac6c63bcb256d32
Skilled5041 <skilled5041@gmail.com>
2024-03-02 10:23:39 -0500

    Added config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,17 @@
-/** @type { import("eslint").Linter.Config } */
 module.exports = {
 	root: true,
 	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:svelte/recommended",
+		"prettier"
 	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
 	parserOptions: {
-		sourceType: 'module',
+		sourceType: "module",
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: [".svelte"]
 	},
 	env: {
 		browser: true,
@@ -21,11 +20,19 @@ module.exports = {
 	},
 	overrides: [
 		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
+				parser: "@typescript-eslint/parser"
 			}
 		}
-	]
+	],
+	rules: {
+		"prefer-const": "warn",
+		"no-var": "error",
+		"no-unused-vars": "warn",
+		"prefer-arrow-callback": "warn",
+		semi: "error",
+		"linebreak-style": ["error", "unix"]
+	}
 };
