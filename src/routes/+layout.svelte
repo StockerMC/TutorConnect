b05a9296b4943d6e39dba2df8e09c4009e765d2b
Skilled5041 <skilled5041@gmail.com>
2024-03-02 15:27:06 -0500

    Added logout button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,21 +2,30 @@
     import "../app.css";
     import { slide } from "svelte/transition";
     import Logo from "$lib/components/Logo.svelte";
+    import { page } from '$app/stores';
 
     export let data;
+    console.log($page.url.pathname);
 </script>
 
-<nav class="h-20 w-full top-0 left-0 bg-gradient-to-br bg-blue-950 flex items-center justify-between px-4">
+<nav class="h-24 w-full top-0 left-0 bg-gradient-to-br bg-black flex items-center justify-between px-4 border-b-2 border-grey">
     <div>
         <Logo/>
     </div>
     <div class="flex items-center space-x-20">
-        <a class="text-white font-bold py-2 rounded-xl hover-fill duration-300 cursor-pointer" href="/">Home</a>
-        <a class="text-white font-bold py-2 rounded-xl hover-fill duration-300 cursor-pointer" href="/listings">Listings</a>
-        <a class="text-white font-bold py-2 rounded-xl hover-fill duration-300 cursor-pointer" href="/create-listing">Create Listing</a>
-        <a class="text-black font-bold px-8 py-3 bg-blue-200 rounded-xl hover-fill-button duration-300" href="/listings">Get Started</a>
+        <a class="text-white font-bold py-2 px-4 hover-fill duration-300 cursor-pointer" href="/"
+           class:border-bottom-white={$page.url.pathname === "/"}>Home</a>
+        <a class="text-white font-bold py-2 px-4 hover-fill duration-300 cursor-pointer" href="/listings"
+           class:border-bottom-white={$page.url.pathname === "/listings"}>Listings</a>
+        <a class="text-white font-bold py-2 px-4 hover-fill duration-300 cursor-pointer" href="/create-listing"
+           class:border-bottom-white={$page.url.pathname === "/create-listing"}>Create
+            Listing</a>
+        <a class="text-black font-bold px-8 py-3 bg-white rounded-xl hover-fill-button duration-300" href="/listings"
+           class:border-bottom-white={$page.url.pathname === "/listings"}>Get
+            Started</a>
         <form method="POST" action="?/logout">
-            <button class="text-black font-bold px-8 py-3 bg-blue-200 rounded-xl hover-fill-button duration-300">Logout</button>
+            <button class="text-black font-bold px-8 py-3 bg-white rounded-xl hover-fill-button duration-300">Logout
+            </button>
         </form>
     </div>
 </nav>
@@ -31,11 +40,20 @@
 <style>
     .hover-fill:hover {
         transition: 0.3s;
-        color: #90b5e7;
+        color: #d9d9d9;
     }
 
     .hover-fill-button:hover {
         transition: 0.3s;
-        background-color: #6da9fc;
+        background-color: #d9d9d9;
+    }
+
+    .border-grey {
+        border-color: #2D2D2D;
+    }
+
+    .border-bottom-white {
+        border-bottom: 2px solid #fff;
+        transition: 0.3s;
     }
 </style>
