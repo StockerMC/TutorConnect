51b3a928b3ba695c0c7a5f8c10cdc9b19c19c2fa
danielp1218 <danielpu2007@gmail.com>
2024-03-02 14:33:13 -0500

    adjust home text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<div class="flex flex-col items-center justify-center h-screen w-screen bg-gray-100">
-	<h1 class="text-9xl font-bold text-gray-800">Home Page</h1>
+<div class="flex flex-col items-center justify-center w-screen bg-gray-100">
+	<h1 class="text-9xl font-bold text-gray-800 pt-40">Home Page</h1>
 </div>
