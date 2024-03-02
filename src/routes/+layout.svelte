f75e8458abca2a748c5b8da04c13cf9c1dfc59a2
Skilled5041 <skilled5041@gmail.com>
2024-03-02 13:42:04 -0500

    restyled navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,10 @@
     export let data;
 </script>
 
-<nav class="h-20 w-screen fixed top-0 left-0 bg-gradient-to-br from-blue-300 to-blue-100">
+<nav class="h-20 w-screen fixed top-0 left-0 bg-gradient-to-br bg-blue-950">
     <div class="flex items-center h-full px-4">
         <a
-                class="text-center text-white font-bold pl-4 pr-4 h-[70%] bg-blue-950 rounded-2xl ml-auto align-middle"
+                class="text-center text-black font-bold pl-4 pr-4 pt-2 pb-2 bg-blue-200 rounded-xl ml-auto align-middle hover-fill duration-300"
                 href="create-listing">Get Started</a
         >
     </div>
@@ -21,4 +21,8 @@
 {/key}
 
 <style>
+    .hover-fill:hover {
+        transition: 0.3s;
+        background: #85a7e3;
+    }
 </style>
