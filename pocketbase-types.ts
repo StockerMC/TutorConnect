78728e93a07391da25f7fe1a2aa74af2c1829aa5
Skilled5041 <skilled5041@gmail.com>
2024-03-02 12:51:08 -0500

    Fix formatting

--- pocketbase-types.ts
+++ pocketbase-types.ts
@@ -1,84 +1,82 @@
 /**
- * This file was @generated using pocketbase-typegen
- */
+* This file was @generated using pocketbase-typegen
+*/
 
-import type PocketBase from "pocketbase";
-import type { RecordService } from "pocketbase";
+import type PocketBase from 'pocketbase'
+import type { RecordService } from 'pocketbase'
 
 export enum Collections {
 	Listings = "listings",
 	Usernames = "usernames",
-	Users = "users"
+	Users = "users",
 }
 
 // Alias types for improved usability
-export type IsoDateString = string;
-export type RecordIdString = string;
-export type HTMLString = string;
+export type IsoDateString = string
+export type RecordIdString = string
+export type HTMLString = string
 
 // System fields
 export type BaseSystemFields<T = never> = {
-	id: RecordIdString;
-	created: IsoDateString;
-	updated: IsoDateString;
-	collectionId: string;
-	collectionName: Collections;
-	expand?: T;
-};
+	id: RecordIdString
+	created: IsoDateString
+	updated: IsoDateString
+	collectionId: string
+	collectionName: Collections
+	expand?: T
+}
 
 export type AuthSystemFields<T = never> = {
-	email: string;
-	emailVisibility: boolean;
-	username: string;
-	verified: boolean;
-} & BaseSystemFields<T>;
+	email: string
+	emailVisibility: boolean
+	username: string
+	verified: boolean
+} & BaseSystemFields<T>
 
 // Record types for each collection
 
 export type ListingsRecord = {
-	description?: string;
-	email?: string;
-	full_name?: string;
-	subjects?: string;
-	title?: string;
-	username: RecordIdString;
-};
+	description?: string
+	email?: string
+	full_name?: string
+	subjects?: string
+	title?: string
+	username?: string
+}
 
 export type UsernamesRecord = {
-	username: string;
-};
+	username: string
+}
 
 export type UsersRecord = {
-	avatar?: string;
-	name?: string;
-};
+	avatar?: string
+	name?: string
+}
 
 // Response types include system fields and match responses from the PocketBase API
-export type ListingsResponse<Texpand = unknown> = Required<ListingsRecord> &
-	BaseSystemFields<Texpand>;
-export type UsernamesResponse<Texpand = unknown> = Required<UsernamesRecord> &
-	BaseSystemFields<Texpand>;
-export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>;
+export type ListingsResponse<Texpand = unknown> = Required<ListingsRecord> & BaseSystemFields<Texpand>
+export type UsernamesResponse<Texpand = unknown> = Required<UsernamesRecord> & BaseSystemFields<Texpand>
+export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
 
 // Types containing all Records and Responses, useful for creating typing helper functions
 
 export type CollectionRecords = {
-	listings: ListingsRecord;
-	usernames: UsernamesRecord;
-	users: UsersRecord;
-};
+	listings: ListingsRecord
+	usernames: UsernamesRecord
+	users: UsersRecord
+}
 
 export type CollectionResponses = {
-	listings: ListingsResponse;
-	usernames: UsernamesResponse;
-	users: UsersResponse;
-};
+	listings: ListingsResponse
+	usernames: UsernamesResponse
+	users: UsersResponse
+}
 
 // Type for usage with type asserted PocketBase instance
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
 export type TypedPocketBase = PocketBase & {
-	collection(idOrName: "listings"): RecordService<ListingsResponse>;
-	collection(idOrName: "usernames"): RecordService<UsernamesResponse>;
-	collection(idOrName: "users"): RecordService<UsersResponse>;
-};
+	collection(idOrName: 'listings'): RecordService<ListingsResponse>
+	collection(idOrName: 'usernames'): RecordService<UsernamesResponse>
+	collection(idOrName: 'users'): RecordService<UsersResponse>
+}
--- src/routes/create-listing/+page.svelte
+++ src/routes/create-listing/+page.svelte
@@ -10,15 +10,10 @@
 		class="flex flex-col items-center justify-center h-screen w-screen bg-gradient-to-br from-blue-100 to-blue-300"
 	>
 		<div
-			class="shadow-2xl flex flex-col justify-center items-center p-7 border-2 rounded-3xl bg-white"
+			class="shadow-2xl flex flex-col justify-center items-center p-24 border-2 rounded-3xl bg-white"
 		>
-			<div class="text-4xl font-bold text-center">Login</div>
-			{#if form?.error}
-				<h1 class="pt-8 text-red-600">
-					Error: username must be 3 to 16 characters (inclusive) and alphanumeric
-				</h1>
-			{/if}
-			<div class="mt-4">
+			<div class="text-4xl font-bold text-center">Create Listing</div>
+			<div class="mt-4 flex flex-col">
 				<input
 					type="text"
 					name="full_name"
