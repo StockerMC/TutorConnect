916d6845ae54d031ea0d46101f034f507d145987
danielp1218 <danielpu2007@gmail.com>
2024-03-02 13:47:58 -0500

    add logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script>
     import "../app.css";
     import { slide } from "svelte/transition";
+    import Logo from "$lib/components/Logo.svelte";
 
     export let data;
 </script>
 
 <nav class="h-20 w-screen top-0 left-0 bg-gradient-to-br bg-blue-950">
+    <div class="absolute top-0 left-0" style="filter: invert(50%) brightness(200%)">
+        <Logo/>
+    </div>
     <div class="flex items-center h-full px-4">
         <a
                 class="text-center text-black font-bold pl-4 pr-4 pt-2 pb-2 bg-blue-200 rounded-xl ml-auto align-middle hover-fill duration-300"
