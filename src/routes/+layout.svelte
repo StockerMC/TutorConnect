9eb4bb2162b0d8e82acf7a692e87405fa08dbdd8
Skilled5041 <skilled5041@gmail.com>
2024-03-02 14:33:00 -0500

    Improved navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,19 +6,18 @@
     export let data;
 </script>
 
-<nav class="h-20 w-screen top-0 left-0 bg-gradient-to-br bg-blue-950">
-    <div class="absolute top-4 left-4">
+<nav class="h-20 w-screen top-0 left-0 bg-gradient-to-br bg-blue-950 flex items-center justify-between px-4">
+    <div>
         <Logo/>
     </div>
-    <div class="flex items-center h-full px-4">
-        <a class="text-center text-white font-bold pl-4 pr-4 pt-2 pb-2 rounded-xl ml-auto align-middle hover-fill duration-300 cursor-pointer"
-           href="/">Home</a>
-        <a
-                class="text-center text-black font-bold pl-4 pr-4 pt-2 pb-2 bg-blue-200 rounded-xl ml-auto align-middle hover-fill duration-300"
-                href="/listings">Get Started</a
-        >
+    <div class="flex items-center space-x-20">
+        <a class="text-white font-bold py-2 rounded-xl hover-fill duration-300 cursor-pointer" href="/">Home</a>
+        <a class="text-white font-bold py-2 rounded-xl hover-fill duration-300 cursor-pointer" href="/listings">Listings</a>
+        <a class="text-white font-bold py-2 rounded-xl hover-fill duration-300 cursor-pointer" href="/create-listing">Create Listing</a>
+        <a class="text-black font-bold px-8 py-3 bg-blue-200 rounded-xl hover-fill-button duration-300" href="/listings">Get Started</a>
     </div>
 </nav>
+
 
 {#key data.pathname}
     <div transition:slide>
@@ -29,6 +28,11 @@
 <style>
     .hover-fill:hover {
         transition: 0.3s;
-        background: #f1f7ff;
+        color: #90b5e7;
+    }
+
+    .hover-fill-button:hover {
+        transition: 0.3s;
+        background-color: #6da9fc;
     }
 </style>
