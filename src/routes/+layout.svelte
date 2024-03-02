8cead937ad1b844b1b0d812aa36cffbd8fb9a9c4
Skilled5041 <skilled5041@gmail.com>
2024-03-02 13:36:32 -0500

    Fixed formatting and added transition animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,24 @@
 <script>
-	import "../app.css";
+    import "../app.css";
+    import { slide } from "svelte/transition";
+
+    export let data;
 </script>
 
 <nav class="h-20 w-screen fixed top-0 left-0 bg-gradient-to-br from-blue-300 to-blue-100">
     <div class="flex items-center h-full px-4">
-        <button class="text-center text-white font-bold pl-4 pr-4 h-[70%] bg-blue-950 rounded-2xl ml-auto align-middle">Get Started</button>
+        <a
+                class="text-center text-white font-bold pl-4 pr-4 h-[70%] bg-blue-950 rounded-2xl ml-auto align-middle"
+                href="create-listing">Get Started</a
+        >
     </div>
 </nav>
 
-<slot />
+{#key data.pathname}
+    <div transition:slide>
+        <slot/>
+    </div>
+{/key}
 
 <style>
-
 </style>
