0417ad9c9acfc143a45f53023c6b53c4fc41590f
Skilled5041 <skilled5041@gmail.com>
2024-03-02 10:46:19 -0500

    Add formatting

--- src/lib/pocketbase-types.ts
+++ src/lib/pocketbase-types.ts
@@ -2,67 +2,68 @@
  * This file was @generated using pocketbase-typegen
  */
 
-import type PocketBase from 'pocketbase'
-import type { RecordService } from 'pocketbase'
+import type PocketBase from "pocketbase";
+import type { RecordService } from "pocketbase";
 
 export enum Collections {
 	Usernames = "usernames",
-	Users = "users",
+	Users = "users"
 }
 
 // Alias types for improved usability
-export type IsoDateString = string
-export type RecordIdString = string
-export type HTMLString = string
+export type IsoDateString = string;
+export type RecordIdString = string;
+export type HTMLString = string;
 
 // System fields
 export type BaseSystemFields<T = never> = {
-	id: RecordIdString
-	created: IsoDateString
-	updated: IsoDateString
-	collectionId: string
-	collectionName: Collections
-	expand?: T
-}
+	id: RecordIdString;
+	created: IsoDateString;
+	updated: IsoDateString;
+	collectionId: string;
+	collectionName: Collections;
+	expand?: T;
+};
 
 export type AuthSystemFields<T = never> = {
-	email: string
-	emailVisibility: boolean
-	username: string
-	verified: boolean
-} & BaseSystemFields<T>
+	email: string;
+	emailVisibility: boolean;
+	username: string;
+	verified: boolean;
+} & BaseSystemFields<T>;
 
 // Record types for each collection
 
 export type UsernamesRecord = {
-	username: string
-}
+	username: string;
+};
 
 export type UsersRecord = {
-	avatar?: string
-	name?: string
-}
+	avatar?: string;
+	name?: string;
+};
 
 // Response types include system fields and match responses from the PocketBase API
-export type UsernamesResponse<Texpand = unknown> = Required<UsernamesRecord> & BaseSystemFields<Texpand>
-export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
+export type UsernamesResponse<Texpand = unknown> = Required<UsernamesRecord> &
+	BaseSystemFields<Texpand>;
+export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>;
 
 // Types containing all Records and Responses, useful for creating typing helper functions
 
 export type CollectionRecords = {
-	usernames: UsernamesRecord
-	users: UsersRecord
-}
+	usernames: UsernamesRecord;
+	users: UsersRecord;
+};
 
 export type CollectionResponses = {
-	usernames: UsernamesResponse
-	users: UsersResponse
-}
+	usernames: UsernamesResponse;
+	users: UsersResponse;
+};
 
 // Type for usage with type asserted PocketBase instance
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
 export type TypedPocketBase = PocketBase & {
-	collection(idOrName: 'usernames'): RecordService<UsernamesResponse>
-	collection(idOrName: 'users'): RecordService<UsersResponse>
-}
+	collection(idOrName: "usernames"): RecordService<UsernamesResponse>;
+	collection(idOrName: "users"): RecordService<UsersResponse>;
+};
--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,9 @@
 import PocketBase from "pocketbase";
-import { SECRET_POCKETBASE_URL, SECRET_POCKETBASE_EMAIL, SECRET_POCKETBASE_PASSWORD } from "$env/static/private";
+import {
+	SECRET_POCKETBASE_URL,
+	SECRET_POCKETBASE_EMAIL,
+	SECRET_POCKETBASE_PASSWORD
+} from "$env/static/private";
 
 export const pocketbase = new PocketBase(SECRET_POCKETBASE_URL);
 await pocketbase.admins.authWithPassword(SECRET_POCKETBASE_EMAIL, SECRET_POCKETBASE_PASSWORD);
