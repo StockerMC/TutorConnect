df819b8aee034fc9a13f28ee360a47ff38e49bd3
danielp1218 <danielpu2007@gmail.com>
2024-03-02 11:51:00 -0500

    create modal.svelte

---
+++ src/lib/components/modal.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let classInfo = "w-[50rem] h-[30rem]";
+
+    export let hidden = false;
+</script>
+
+<div class="fixed top-0 left-0 w-screen h-screen flex justify-center items-center z-50" style="background-color: rgba(0,0,0,0.2)" class:hidden={hidden}>
+    <div class="relative shadow-2xl bg-white rounded-3xl {classInfo} z-50 pt-3">
+        <span class="absolute top-2 right-4 text-black text-4xl select-none x-button" on:pointerdown={()=>(hidden=true)}>x</span>
+    </div>
+</div>
+
+<style>
+    .x-button {
+        cursor: pointer;
+        transition: all 0.1s linear;
+    }
+    .x-button:hover {
+        color: rgb(128, 128, 128);
+        transition: all 0.1s linear;
+    }
+</style>
